307dbb17700b1a03416bca80704100cf00701cf7
Nico Hrubec <nicolas.hrubec@outlook.com>
2020-01-20 10:29:11 +0100

    Baseline preprocesing pipeline implemented.
    Returns a numpy array of shape (num_samples, max_num_atoms, num_features_per_atom).
    For now only features are the relative x,y,z coordinates for all atoms
    relative to the calculated bond center (mean coordinates of the atoms in the bond).
    All molecules that are smaller than 29 atoms (max atoms) are padded with 0s.
    Only other feature than the rel coordinates is an indicator if the atom row
    in a sample is padding or not.

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
 from src import preprocessing
 
-train, test = preprocessing.do_preprocessing(debug=True)
+train, test, target, structures = preprocessing.do_preprocessing(debug=True)
+train, test = preprocessing.do_engineering(train, test, structures)
+
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 from src import configs
 
@@ -6,9 +7,52 @@ from src import configs
 def do_preprocessing(debug=False):
     train = pd.read_csv(configs.train, index_col='id')
     test = pd.read_csv(configs.test, index_col='id')
+    structures = pd.read_csv(configs.structures).set_index('molecule_name')
 
     if debug:
         train = train.sample(frac=.01, random_state=42)
         test = test.sample(frac=.01, random_state=42)
 
+    target = train.pop('scalar_coupling_constant')
+
+    return train, test, target, structures
+
+
+def do_engineering(train, test, structures):
+    train = get_representation(train, structures)
+    test = get_representation(test, structures)
+
     return train, test
+
+
+def get_representation(df, structures):
+    new_df = np.zeros((len(df), 29 * 4))  # (number of samples, max atoms in molecule x number of features)
+
+    for index, sample in enumerate(df.iterrows()):
+        sample_df = np.zeros((29, 4))
+
+        # retrieve the molecule name for a particular bond
+        data = sample[1]  # iterrows returns tuple of (index, data)
+        mol = data[['molecule_name']].values[0]
+
+        # get x y z coordinates of all the atoms in the given molecule as np array
+        mol_structure = structures.loc[mol]
+        num_atoms = len(mol_structure)
+        mol_structure = mol_structure[['x', 'y', 'z']].values
+
+        # get coordinates of the two atoms in the bond
+        atom1 = mol_structure[data[['atom_index_0']].values[0]]
+        atom2 = mol_structure[data[['atom_index_1']].values[0]]
+
+        # calculate center by taking the mean of the coordinates of the atoms in the bond
+        bond_center = np.divide(np.add(atom1, atom2), 2)
+        mol_structure = np.subtract(mol_structure, bond_center)
+        mol_structure = np.hstack((mol_structure, np.ones((num_atoms, 1))))  # indicator that these rows correspond to an atom not padding
+        sample_df[:num_atoms] = mol_structure
+
+        sample_df = sample_df.flatten()
+        new_df[index] = sample_df
+
+    new_df = np.reshape(new_df, (len(df), 29, 4))
+
+    return new_df
