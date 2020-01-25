51e2167a16365993894c727791faeea19a31fe44
Nico Hrubec <nicolas.hrubec@outlook.com>
2020-01-25 10:16:48 +0100

    Decrease model capacity.
    Back to a smaller model and first improve molecule representation.

--- src/model.py
+++ src/model.py
@@ -9,15 +9,12 @@ class PointCNN(nn.Module):
         # input (batch_size, num_max_atoms, num_features)
         self.conv1 = nn.Conv1d(in_channels=29, out_channels=512, kernel_size=1)
         self.relu = nn.ReLU()
-        self.conv2 = nn.Conv1d(in_channels=512, out_channels=256, kernel_size=1)
         self.pool = nn.AvgPool1d(kernel_size=4)
-        self.dense1 = nn.Linear(256, 128)
-        self.dense2 = nn.Linear(128, 1)
+        self.dense1 = nn.Linear(512, 256)
+        self.dense2 = nn.Linear(256, 1)
 
     def forward(self, x):
         x = self.conv1(x)
-        x = self.relu(x)
-        x = self.conv2(x)
         x = self.relu(x)
         x = self.pool(x)
         x = x.view(x.shape[0], x.shape[1])
