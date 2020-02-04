1d1ef9895b3cdfb90221ac01548cff22467b4e81
Nico Hrubec <nicolas.hrubec@outlook.com>
2020-02-04 09:57:16 +0100

    Fix: Input now in right format.
    Features are now handled as channels.
    Adapted neural net accordingly.

--- src/model.py
+++ src/model.py
@@ -6,12 +6,12 @@ class PointCNN(nn.Module):
 
     def __init__(self):
         super(PointCNN, self).__init__()
-        # input (batch_size, num_max_atoms, num_features)
-        self.conv1 = nn.Conv1d(in_channels=29, out_channels=512, kernel_size=1)
+        # input (batch_size, num_features, num_atoms)
+        self.conv1 = nn.Conv1d(in_channels=9, out_channels=512, kernel_size=1)
         self.conv2 = nn.Conv1d(in_channels=512, out_channels=256, kernel_size=1)
         self.conv3 = nn.Conv1d(in_channels=256, out_channels=128, kernel_size=1)
         self.relu = nn.ReLU()
-        self.pool = nn.AvgPool1d(kernel_size=9)
+        self.pool = nn.AvgPool1d(kernel_size=29)
         self.dense1 = nn.Linear(128, 1024)
         self.dense2 = nn.Linear(1024, 4096)
         self.dense3 = nn.Linear(4096, 2048)
@@ -19,6 +19,7 @@ class PointCNN(nn.Module):
         self.dense5 = nn.Linear(256, 1)
 
     def forward(self, x):
+        x = x.permute(0, 2, 1) # reshape so that channels are second axis in the tensor
         x = self.conv1(x)
         x = self.relu(x)
         x = self.conv2(x)
